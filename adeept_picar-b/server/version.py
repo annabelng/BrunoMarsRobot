9921f28101ab481e9d46d2124ceb958e7f4b544e
Yufei Zhang <yfzhang819@gmail.com>
2021-06-03 10:28:48 -0700

    Update version.py

--- adeept_picar-b/server/version.py
+++ adeept_picar-b/server/version.py
@@ -1,4 +1,4 @@
-import speech_recognition as sr;print(sr.__version__)
-import pyaudio as p;print(p.__version__)
-
+#import speech_recognition as sr;print(sr.__version__)
+#import pyaudio as p;print(p.__version__)
+import speech_recognition as sr
 print(sr.Microphone.list_microphone_names())
