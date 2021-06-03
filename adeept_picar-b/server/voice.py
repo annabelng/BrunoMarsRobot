d19f9cc20a842a1f5fb45684a890e5ed6727c4bb
Yufei Zhang <yfzhang819@gmail.com>
2021-06-03 14:39:44 -0700

    update voice

--- adeept_picar-b/server/voice.py
+++ adeept_picar-b/server/voice.py
@@ -1,7 +1,14 @@
 import speech_recognition as sr
 import time
-#import move
-#move.setup()
+
+import RPIservo
+
+
+scGear = RPIservo.ServoCtrl()
+scGear.moveInit()
+
+import move
+move.setup()
 # obtain audio from the microphone
 #r = sr.Recognizer()
 #mic = sr.Microphone(device_index=2)
@@ -71,18 +78,26 @@ except sr.RequestError as e:
 
 if __name__ == "__main__":
     # set the list of words, maxnumber of guesses, and prompt limit
-    WORDS = ["apple", "banana", "grape", "orange", "mango", "lemon"]
-    NUM_GUESSES = 3
-    PROMPT_LIMIT = 3
+    WORDS = ["forward", "backward", "left", "right"]
+    NUM_COMMANDS = 5 #number of tries
+    RETRY_LIMIT = 3 #didn't transcribe, try again
     # create recognizer and mic instances
     recognizer = sr.Recognizer()
     microphone = sr.Microphone()
 
     # show instructions and wait 1 seconds before starting the game
+   # format the instructions string
+    instructions = (
+        "Here are the commands:\n"
+        "{words}\n"
+        "You have {n} commands to give\n"
+    ).format(words=', '.join(WORDS), n=NUM_COMMANDS)
 
-    time.sleep(1)
+    # show instructions and wait 3 seconds before starting the game
+    print(instructions)
+    time.sleep(3)
 
-    for i in range(NUM_GUESSES):
+    for i in range(NUM_COMMANDS):
         # get the guess from the user
         # if a transcription is returned, break out of the loop and
         #     continue
@@ -91,7 +106,7 @@ if __name__ == "__main__":
         # if API request succeeded but no transcription was returned,
         #     re-prompt the user to say their guess again. Do this up
         #     to PROMPT_LIMIT times
-        for j in range(PROMPT_LIMIT):
+        for j in range(RETRY_LIMIT):
             print('Guess {}. Speak!'.format(i+1))
             command = recognize_speech_from_mic(recognizer, microphone)
             if command["transcription"]:
@@ -111,19 +126,33 @@ if __name__ == "__main__":
         # determine if guess is correct and if any attempts remain
         go_forwards = command["transcription"].lower() == "forward"
         go_backwards = command["transcription"].lower() == "backwards"
-        go_forwards = command["transcription"].lower() == "forward"
-        go_forwards = command["transcription"].lower() == "forward"
+        go_left = command["transcription"].lower() == "forward"
+        go_right = command["transcription"].lower() == "forward"
 
-        user_has_more_attempts = i < NUM_GUESSES - 1
+        user_has_more_attempts = i < NUM_COMMANDS - 1
 
         # determine if the user has won the game
         # if not, repeat the loop if user has more attempts
         # if no attempts left, the user loses the game
         if go_forwards:
             print("Going forwards")
+            scGear.moveAngle(2, 0)
+            move.motor_left(1, 0, 50)
+            move.motor_right(1, 0, 50)
+            time.sleep(2)
+            move.motorStop()
             break
         if go_backwards:
             print("Going backwards")
+            
+            break
+        if go_left:
+            print("Going left")
+            
+            break
+        if go_right:
+            print("Going right")
+        
             break
         else:
             print("Sorry, not a command")
