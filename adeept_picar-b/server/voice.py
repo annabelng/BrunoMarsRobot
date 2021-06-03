2b777c10f8908a73ee8bf220bb4d0ec7979b1897
Yufei Zhang <yfzhang819@gmail.com>
2021-06-03 10:53:49 -0700

    Update voice.py

--- adeept_picar-b/server/voice.py
+++ adeept_picar-b/server/voice.py
@@ -1,13 +1,64 @@
 import speech_recognition as sr
-
+import time
 #import move
 #move.setup()
 # obtain audio from the microphone
 r = sr.Recognizer()
-mic = sr.Microphone()
-sr.Microphone.list_microphone_names()
-with sr.Microphone() as source: #using laptop mic not ROBO CAR!!
+mic = sr.Microphone(device_index=2)
+
+def recognize_speech_from_mic(recognizer, microphone):
+    """Transcribe speech from recorded from `microphone`.
+
+    Returns a dictionary with three keys:
+    "success": a boolean indicating whether or not the API request was
+               successful
+    "error":   `None` if no error occured, otherwise a string containing
+               an error message if the API could not be reached or
+               speech was unrecognizable
+    "transcription": `None` if speech could not be transcribed,
+               otherwise a string containing the transcribed text
+    """
+    # check that recognizer and microphone arguments are appropriate type
+    if not isinstance(recognizer, sr.Recognizer):
+        raise TypeError("`recognizer` must be `Recognizer` instance")
+
+    if not isinstance(microphone, sr.Microphone):
+        raise TypeError("`microphone` must be `Microphone` instance")
+
+    # adjust the recognizer sensitivity to ambient noise and record audio
+    # from the microphone
+    with microphone as source:
+        recognizer.adjust_for_ambient_noise(source)
+        audio = recognizer.listen(source)
+
+    # set up the response object
+    response = {
+        "success": True,
+        "error": None,
+        "transcription": None
+    }
+
+    # try recognizing the speech in the recording
+    # if a RequestError or UnknownValueError exception is caught,
+    #     update the response object accordingly
+    try:
+        response["transcription"] = recognizer.recognize_google(audio)
+    except sr.RequestError:
+        # API was unreachable or unresponsive
+        response["success"] = False
+        response["error"] = "API unavailable"
+    except sr.UnknownValueError:
+        # speech was unintelligible
+        response["error"] = "Unable to recognize speech"
+
+    return response
+
+recognize_speech_from_mic(r, mic)
+
+'''
+with mic as source: #using laptop mic not ROBO CAR!!
     print("Say something!")
+    r.adjust_for_ambient_noise()
     audio = r.listen(source)
     v_command = r.recognize_google(audio)
 # recognize speech using Google Speech Recognition
@@ -17,8 +68,67 @@ except sr.UnknownValueError:
     print("Google Speech Recognition could not understand audio")
 except sr.RequestError as e:
     print("Could not request results from Google Speech Recognition service; {0}".format(e))
+'''
+
+if __name__ == "__main__":
+    # set the list of words, maxnumber of guesses, and prompt limit
+    WORDS = ["apple", "banana", "grape", "orange", "mango", "lemon"]
+    NUM_GUESSES = 3
+    PROMPT_LIMIT = 3
+    # create recognizer and mic instances
+    recognizer = sr.Recognizer()
+    microphone = sr.Microphone()
+
+    # show instructions and wait 1 seconds before starting the game
+
+    time.sleep(1)
+
+    for i in range(NUM_GUESSES):
+        # get the guess from the user
+        # if a transcription is returned, break out of the loop and
+        #     continue
+        # if no transcription returned and API request failed, break
+        #     loop and continue
+        # if API request succeeded but no transcription was returned,
+        #     re-prompt the user to say their guess again. Do this up
+        #     to PROMPT_LIMIT times
+        for j in range(PROMPT_LIMIT):
+            print('Guess {}. Speak!'.format(i+1))
+            command = recognize_speech_from_mic(recognizer, microphone)
+            if command["transcription"]:
+                break
+            if not command["success"]:
+                break
+            print("I didn't catch that. What did you say?\n")
+
+        # if there was an error, stop the game
+        if command["error"]:
+            print("ERROR: {}".format(command["error"]))
+            break
+
+        # show the user the transcription
+        print("You said: {}".format(command["transcription"]))
+
+        # determine if guess is correct and if any attempts remain
+        go_forwards = command["transcription"].lower() == "forward"
+        user_has_more_attempts = i < NUM_GUESSES - 1
+
+        # determine if the user has won the game
+        # if not, repeat the loop if user has more attempts
+        # if no attempts left, the user loses the game
+        if go_forwards:
+            print("Going forwards")
+            break
+        elif 'backward' in v_command:
+            print("moving backwards")
+        else:
+            print("Sorry, not a command")
+            break
 
 
+
+
+""""
 if 'forward' in v_command:
   print("moving forward")
   #scGear.moveAngle(2, 0)
@@ -56,3 +166,4 @@ elif 'stop' in v_command:
    #move.motorStop()  else:
    pass
  
+"""
